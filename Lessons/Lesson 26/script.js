30007c8ed75ae2ea486288a113e63be8e7e383c1
Alex <yarmoshsasha@mail.ru>
2021-01-26 14:30:43 +0800

    Dynamic typing in JS

--- Lessons/Lesson 26/script.js
+++ Lessons/Lesson 26/script.js
@@ -1,2 +1,52 @@
 "use sctrict";
 
+// To string
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(null + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+// To Number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'5'));
+
+// 3)
+console.log(typeof(parseInt("15px", 10)));
+
+
+let answ = +prompt("Hello", "");
+
+// To boolean
+
+// 0, '', null, undefiend, NaN; --> false
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!"33333"));
